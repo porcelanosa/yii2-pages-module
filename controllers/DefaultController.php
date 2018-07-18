bd3d40eccb8a921ea8215c8fdc986c419be10d56
porcelanosa <public_mce@rambler.ru>
2018-07-18 12:23:28 +0300

    Before resuming work

--- controllers/DefaultController.php
+++ controllers/DefaultController.php
@@ -3,9 +3,10 @@
     namespace common\modules\pages\controllers;
     
     use common\modules\pages\models\Pages;
+    use backend\controllers\BasebackendController as Controller;
     use yii\filters\AccessControl;
     use yii\filters\VerbFilter;
-    use yii\web\Controller;
+    //use yii\web\Controller;
     use yii\web\NotFoundHttpException;
     
     /**
@@ -13,49 +14,6 @@
      */
     class DefaultController extends Controller
     {
-        /**
-         * @inheritdoc
-         */
-        public function behaviors()
-        {
-            return [
-                'access' => [
-                    'class' => AccessControl::className(),
-                    'only'  => ['logout', 'signup'],
-                    'rules' => [
-                        [
-                            'actions' => ['signup'],
-                            'allow'   => true,
-                            'roles'   => ['?'],
-                        ],
-                        [
-                            'actions' => ['logout'],
-                            'allow'   => true,
-                            'roles'   => ['@'],
-                        ],
-                    ],
-                ],
-                'verbs'  => [
-                    'class'   => VerbFilter::className(),
-                    'actions' => [
-                        'logout' => ['post'],
-                    ],
-                ],
-            ];
-        }
-        
-        /**
-         * @inheritdoc
-         */
-        public function actions()
-        {
-            return [
-                'error' => [
-                    'class' => 'yii\web\ErrorAction',
-                ],
-            ];
-        }
-        
         /**
          * Renders the index view for the module
          *
